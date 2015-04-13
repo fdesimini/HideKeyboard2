ef3f9dbdc8a36ea4f1a2ccb89c001cf4d5f02cd6
fdesimini <fdesimini@users.noreply.github.com>
2015-04-13 17:33:49 -0400

    Initial Commit

---
+++ HideKeyboard2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HideKeyboard2
+//
+//  Created by Frank Desimini on 2015-04-13.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
