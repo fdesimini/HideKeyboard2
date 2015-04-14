6f10b968c87c9fcdb9a2d933c76f6e4140ca11b7
fdesimini <fdesimini@users.noreply.github.com>
2015-04-13 21:04:07 -0400

    all features in place

--- HideKeyboard2/ViewController.h
+++ HideKeyboard2/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *textField;
 
+- (IBAction)textFieldReturn:(id)sender;
 
 @end
 
